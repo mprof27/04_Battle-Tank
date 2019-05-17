cfa9004bbcf7b9dccbaec50aa3eda18b1796ef34
mprof27 <mark.sengr@gmail.com>
2019-05-17 20:03:18 +0900

    BT22 Using DeprojectScreenToWorld

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -42,7 +42,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation;	// Out parameter
 	if (GetSightRayHitLocation(HitLocation))	// Has "side-effect", is going to line trace
 	{
-	//UE_LOG(LogTemp, Warning, TEXT("Look direcion: %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look direcion: %s"), *HitLocation.ToString());
 		// TODO Tell controlled tank to aim at this point
 	}
 }
@@ -52,10 +52,27 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 {
 	// Find the crosshair position in pixel coordinates
 	int32 ViewportSizeX, ViewportSizeY;
-	GetViewportSize(ViewportSizeX, ViewportSizeY);	
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
-	//UE_LOG(LogTemp, Warning, TEXT("screenLocation: %s"), *ScreenLocation.ToString);
+
 	// "De-project" the screen position of the crosshair to a world directiion
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+	}
+
 	// Line-trace along that look direction, and see wha we hit (up to max range)
 	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation;	// To be discarded
+	return DeprojectScreenPositionToWorld(
+		ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection
+	);
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -29,7 +29,11 @@ private:
 	// Return an OUT parameter, true if his landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
+	UPROPERTY(EditAnywhere)
 	float CrossHairXLocation = 0.5;
 
+	UPROPERTY(EditAnywhere)
 	float CrossHairYLocation = 0.3333;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 };
