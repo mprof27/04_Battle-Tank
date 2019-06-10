e460756d29443b6f3a32d515b3ed9418048b320f
mprof27 <mark.sengr@gmail.com>
2019-06-10 15:24:57 +0900

    BT46 A Better Component Architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,12 +2,23 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 #include "BattleTank.h"
 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	// TODO prevent double-speed due to dual control use
 }
 
